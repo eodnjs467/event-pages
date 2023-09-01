43a93276337a410e9a9246dd8132cdee62874d8a
Big One <sponjjanc@naver.com>
2023-09-02 00:31:42 +0900

    feat: link is exist or non-link

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import styles from './button.module.css';
 function Button(props) {
+  if(props.link) return <Link className={styles.btn} href={props.link}>{props.children}</Link>;
+
   return(
-      <Link className={styles.btn} href={props.link}>{props.children}</Link>
+      <button className={styles.btn} onClick={props.onClick}>{props.children}</button>
   )
 }
 export default Button
