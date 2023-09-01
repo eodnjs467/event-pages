01db403cf5c717dc0a180e87528668c4d77ed278
Big One <sponjjanc@naver.com>
2023-09-02 00:33:00 +0900

    feat: filtered event slug page

--- src/app/events/[...slug]/page.js
+++ src/app/events/[...slug]/page.js
@@ -1,5 +1,25 @@
-export default function FilteredEventsPage() {
-  return(
-      <div>Filtered PAge</div>
-  )
+import EventList from "../../../components/events/eventList";
+import {getFilteredEvents} from "../../../../dummy-data";
+export default function FilteredEventsPage({params}) {
+  const filteredData = params.slug;
+
+  if(!filteredData){
+    return <p className="center">Loading...</p>
+  }
+
+  const filteredYear = +filteredData[0];
+  const filteredMonth = +filteredData[1];
+
+  if(isNaN(filteredYear) || isNaN(filteredMonth) || filteredYear > 2030 || filteredMonth > 12) {
+    return <p>Invalid filter. Please adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({year: filteredYear, month: filteredMonth});
+
+  return (
+      <div>
+        <EventList items={filteredEvents} />
+      </div>
+  );
 }
+
