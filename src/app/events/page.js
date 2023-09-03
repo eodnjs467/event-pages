d6c279812092febacff5eaa7412e4b752f18ed36
Big One <sponjjanc@naver.com>
2023-09-03 22:53:54 +0900

    feat: all events page

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,12 +1,20 @@
-import {getFeaturedEvents} from "../../../dummy-data";
-import EventList from "../../components/events/EventList";
+"use client"
+import {getAllEvents} from "../../../dummy-data";
+import EventList from "../../components/events/eventList";
+import EventsSearch from "../../components/events/eventsSearch";
+import {useRouter} from "next/navigation";
 
 export default function EventsPage() {
-  const featuredEvents = getFeaturedEvents();
-  console.log(featuredEvents)
+  const router = useRouter();
+  const events = getAllEvents();
+  const findEventsHandler = (year, month) => {
+    const pullPath = `/events/${year}/${month}`;
+    router.push(pullPath);
+  }
   return(
         <div>
-          <EventList events={featuredEvents} />
+          <EventsSearch onSearch={findEventsHandler}/>
+          <EventList items={events} />
         </div>
   )
 }
