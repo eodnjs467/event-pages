9fbb74e785eb0bc0b951a6cf12d80fa545cc3e08
Big One <sponjjanc@naver.com>
2023-09-03 22:54:43 +0900

    feat: results-title component

---
+++ src/components/events/resultsTitle.js
@@ -0,0 +1,16 @@
+import Button from "../ui/button";
+import styles from './resultsTitle.module.css';
+function ResultsTitle(props) {
+  const { date } = props;
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    month: 'long',
+    year: 'numeric',
+  })
+  return (
+      <section className={styles.title}>
+        <h1>Events in {humanReadableDate}</h1>
+        <Button link={"/events"}>Show all events</Button>
+      </section>
+  );
+}
+export default ResultsTitle;
