075832eb5efba819e60e6a144c23c41a26448d3b
Big One <sponjjanc@naver.com>
2023-09-03 22:53:37 +0900

    feat: events detail page

--- src/app/events/[id]/page.js
+++ src/app/events/[id]/page.js
@@ -1,7 +1,26 @@
+import {getEventById} from "../../../../dummy-data";
+import EventSummary from "../../../components/events/eventSummary";
+import EventLogistics from "../../../components/events/eventLogistics";
+import EventContent from "../../../components/events/eventContent";
+
 export default function EventDetailPage({ params }){
+  const eventId = params.id;
+  const event = getEventById(eventId);
+  if(!event) {
+    return <p>No event found!</p>;
+  }
   return(
-      <div>
-        Detail Page: {params.id}
-      </div>
+      <>
+        <EventSummary title={event.title}/>
+        <EventLogistics
+            date={event.date}
+            address={event.location}
+            image={event.image}
+            imageAlt={event.title}
+        />
+        <EventContent>
+          <p>{event.description}</p>
+        </EventContent>
+      </>
   )
 }
