dcf3736abc4db7ab44500b3a7bb46ef7268669b7
Big One <sponjjanc@naver.com>
2023-10-24 21:45:19 +0900

    test: push action test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
 import { getFeaturedEvents } from '../../dummy-data';
 import EventList from '../components/events/eventList';
-export default function HomePage() {
-  const featuredEvents = getFeaturedEvents();
-  return (
-    <main>
-      <EventList items={featuredEvents} />
-    </main>
+
+const options = {
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN}`,
+  },
+};
+export default async function HomePage() {
+  const data = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?language=${'ko'}&page=${1}`,
+      options,
   )
+      .then((response) => response.json())
+  console.log(data);
+  const featuredEvents = getFeaturedEvents();
+  const text = process.env.NEXT_PUBLIC_TOKEN;
+  console.log(text);
+  return (
+      <main>
+        <div>텍스트는? : {text}</div>
+
+        <EventList items={featuredEvents}/>
+      </main>
+  );
 }
