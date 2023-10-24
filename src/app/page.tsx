3f50699e5dd34ade2730b5c11ffbf8b0e67d6b26
Big One <sponjjanc@naver.com>
2023-10-24 20:49:46 +0900

    test: push action test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@ import { getFeaturedEvents } from '../../dummy-data';
 import EventList from '../components/events/eventList';
 export default function HomePage() {
   const featuredEvents = getFeaturedEvents();
+  const text = process.env.NEXT_PUBLIC_TOKEN;
+  console.log(text);
   return (
-    <main>
-      <EventList items={featuredEvents} />
-    </main>
-  )
+      <main>
+        <div>텍스트: {text}</div>
+        <EventList items={featuredEvents}/>
+      </main>
+  );
 }
